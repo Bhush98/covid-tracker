da808328fd70996c18e0205eeb25baeb51fa79f3
Bhushan Rajput <bhushanrajput2298@gmail.com>
2020-07-05 17:02:37 +0530

    Updates

---
+++ precache-manifest.25ef2ef16440d205ea20c135c82cbb93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ac7ef39bb28dcb3227ba13c6138ef1",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "d5880dc534449e762dd4",
+    "url": "/covid-tracker/static/css/main.77a3032a.chunk.css"
+  },
+  {
+    "revision": "5ab0648b768e89c0d927",
+    "url": "/covid-tracker/static/js/2.6f625554.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid-tracker/static/js/2.6f625554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5880dc534449e762dd4",
+    "url": "/covid-tracker/static/js/main.4aaaaf7c.chunk.js"
+  },
+  {
+    "revision": "b48ad7191357c4c02390",
+    "url": "/covid-tracker/static/js/runtime-main.a80033f0.js"
+  }
+]);
